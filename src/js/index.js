06dae23d009196f152e270fe7080a03ba337dbbb
Marizze Ponce <Poncemarizze@gmail.com>
2024-05-06 01:58:08 +0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("#header").load('/src/templates/header.html');
+    $("#main").load('/src/templates/home.html');
+    $("#footer").load('/src/templates/footer.html');
+})
+
+/////observer
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.animate');
+hiddenElements.forEach((el) => observer.observe(el));
+
