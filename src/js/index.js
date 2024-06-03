7cce9b43afb85e129d468378ce4051d575f489a3
2nie Garcia <garciaantoniojr190@gmail.com>
2024-06-03 11:53:57 +0800

    +features

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,51 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.animate');
 hiddenElements.forEach((el) => observer.observe(el));
+
+//image view
+
+function show() {
+  /* Access image by id and change 
+  the display property to block*/
+  document.getElementById('show')
+      .style.display = "block";
+  document.getElementById('btnID')
+      .style.display = "none";
+}
+
+//modal
+
+const myModalEl = document.getElementById('myModal')
+myModalEl.addEventListener('hidden.bs.modal', event => {
+  // do something...
+})
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+const myModalAlternative = new bootstrap.Modal('#myModal', options)
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
